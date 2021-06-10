b34522d16f1b706cc8bd597817546bde98fea796
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2021-06-10 19:39:00 +0100

    updated

--- linechart.js
+++ linechart.js
@@ -1,119 +1,66 @@
-function linechart(data_json) {
+function linechart(data) {
+
+    var parseTime = d3.timeParse("%d-%b-%y");
 
 
-    var svg = d3.select("line_chart"),
-        margin = {top: 20, right: 20, bottom: 30, left: 40},
-        width = +svg.attr("width") - margin.left - margin.right,
-        height = +svg.attr("height") - margin.top - margin.bottom;
+    // format the data
+    data.forEach(function (d) {
+        d.date = parseTime(d.date);
+        d.close = +d.close;
+    });
 
-    var parseTime = d3.timeParse("%Y")
-    bisectDate = d3.bisector(function (d) {
-        return d.year;
-    }).left;
+    // set the dimensions and margins of the graph
+    var margin = {top: 20, right: 20, bottom: 30, left: 50},
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
 
+
+    // set the ranges
     var x = d3.scaleTime().range([0, width]);
     var y = d3.scaleLinear().range([height, 0]);
 
-    var line = d3.line()
+    // define the line
+    var valueline = d3.line()
         .x(function (d) {
-            return x(d.year);
+            return x(d.date);
         })
         .y(function (d) {
-            return y(d.value);
+            return y(d.close);
         });
 
-    var g = svg.append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    // append the svg obgect to the body of the page
+    // appends a 'group' element to 'svg'
+    // moves the 'group' element to the top left margin
+    var svg = d3.select("#line-chart").select("svg")
+        // d3.select("body").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
 
-    d3.json(data_json, function (error, data) {
-        if (error) throw error;
 
-        data.forEach(function (d) {
-            d.year = parseTime(d.year);
-            d.value = +d.value;
-        });
+    // Scale the range of the data
+    x.domain(d3.extent(data, function (d) {
+        return d.date;
+    }));
+    y.domain([0, d3.max(data, function (d) {
+        return d.close;
+    })]);
 
-        x.domain(d3.extent(data, function (d) {
-            return d.year;
-        }));
-        y.domain([d3.min(data, function (d) {
-            return d.value;
-        }) / 1.005, d3.max(data, function (d) {
-            return d.value;
-        }) * 1.005]);
+    // Add the valueline path.
+    svg.append("path")
+        .data([data])
+        .attr("class", "line")
+        .attr("d", valueline);
 
-        g.append("g")
-            .attr("class", "axis axis--x")
-            .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x));
+    // Add the X Axis
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
 
-        g.append("g")
-            .attr("class", "axis axis--y")
-            .call(d3.axisLeft(y).ticks(6).tickFormat(function (d) {
-                return parseInt(d / 1000) + "k";
-            }))
-            .append("text")
-            .attr("class", "axis-title")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 6)
-            .attr("dy", ".71em")
-            .style("text-anchor", "end")
-            .attr("fill", "#5D6971")
-            .text("Population)");
-
-        g.append("path")
-            .datum(data)
-            .attr("class", "line")
-            .attr("d", line);
-
-        var focus = g.append("g")
-            .attr("class", "focus")
-            .style("display", "none");
-
-        focus.append("line")
-            .attr("class", "x-hover-line hover-line")
-            .attr("y1", 0)
-            .attr("y2", height);
-
-        focus.append("line")
-            .attr("class", "y-hover-line hover-line")
-            .attr("x1", width)
-            .attr("x2", width);
-
-        focus.append("circle")
-            .attr("r", 7.5);
-
-        focus.append("text")
-            .attr("x", 15)
-            .attr("dy", ".31em");
-
-        svg.append("rect")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-            .attr("class", "overlay")
-            .attr("width", width)
-            .attr("height", height)
-            .on("mouseover", function () {
-                focus.style("display", null);
-            })
-            .on("mouseout", function () {
-                focus.style("display", "none");
-            })
-            .on("mousemove", mousemove);
-
-        function mousemove() {
-            var x0 = x.invert(d3.mouse(this)[0]),
-                i = bisectDate(data, x0, 1),
-                d0 = data[i - 1],
-                d1 = data[i],
-                d = x0 - d0.year > d1.year - x0 ? d1 : d0;
-            focus.attr("transform", "translate(" + x(d.year) + "," + y(d.value) + ")");
-            focus.select("text").text(function () {
-                return d.value;
-            });
-            focus.select(".x-hover-line").attr("y2", height - y(d.value));
-            focus.select(".y-hover-line").attr("x2", width + width);
-        }
-    });
-
+    // Add the Y Axis
+    svg.append("g")
+        .call(d3.axisLeft(y));
 
 }
