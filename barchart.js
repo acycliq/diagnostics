78f74bc8a3e64649d7474aea3f7a745dda8e2f9c
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2021-06-10 21:07:14 +0100

    rotated x-labels on the barchart

--- barchart.js
+++ barchart.js
@@ -1,66 +1,72 @@
-function barchart(data){
+function barchart(data) {
 
-       // set the dimensions and margins of the graph
-        var margin = {top: 20, right: 20, bottom: 30, left: 40},
-            width = 960 - margin.left - margin.right,
-            height = 500 - margin.top - margin.bottom;
+    // set the dimensions and margins of the graph
+    var margin = {top: 20, right: 20, bottom: 70, left: 40},
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
 
-        // set the ranges
-        var x = d3.scaleBand()
-            .range([0, width])
-            .padding(0.1);
-        var y = d3.scaleLinear()
-            .range([height, 0]);
+    // set the ranges
+    var x = d3.scaleBand()
+        .range([0, width])
+        .padding(0.1);
+    var y = d3.scaleLinear()
+        .range([height, 0]);
 
-        // append the svg object to the body of the page
-        // append a 'group' element to 'svg'
-        // moves the 'group' element to the top left margin
-        var svg = d3.select("#bar-chart").select("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform",
-                "translate(" + margin.left + "," + margin.top + ")");
-        // if (error) throw error;
+    // append the svg object to the body of the page
+    // append a 'group' element to 'svg'
+    // moves the 'group' element to the top left margin
+    var svg = d3.select("#bar-chart").select("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+    // if (error) throw error;
 
-        // format the data
-        data.forEach(function (d) {
-            d.Prob = +d.Prob;
+    // format the data
+    data.forEach(function (d) {
+        d.Prob = +d.Prob;
+    });
+
+    // Scale the range of the data in the domains
+    x.domain(data.map(function (d) {
+        return d.Class;
+    }));
+    y.domain([0, d3.max(data, function (d) {
+        return d.Prob;
+    })]);
+
+    // append the rectangles for the bar chart
+    svg.selectAll(".bar")
+        .data(data)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function (d) {
+            return x(d.Class);
+        })
+        .attr("width", x.bandwidth())
+        .attr("y", function (d) {
+            return y(d.Prob);
+        })
+        .attr("height", function (d) {
+            return height - y(d.Prob);
         });
 
-        // Scale the range of the data in the domains
-        x.domain(data.map(function (d) {
-            return d.Class;
-        }));
-        y.domain([0, d3.max(data, function (d) {
-            return d.Prob;
-        })]);
+    // add the x Axis
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x))
+        .selectAll("text")
+        .style("text-anchor", "end")
+        .attr("dx", "-.8em")
+        .attr("dy", ".15em")
+        .attr("transform", "rotate(-60)");
 
-        // append the rectangles for the bar chart
-        svg.selectAll(".bar")
-            .data(data)
-            .enter().append("rect")
-            .attr("class", "bar")
-            .attr("x", function (d) {
-                return x(d.Class);
-            })
-            .attr("width", x.bandwidth())
-            .attr("y", function (d) {
-                return y(d.Prob);
-            })
-            .attr("height", function (d) {
-                return height - y(d.Prob);
-            });
 
-        // add the x Axis
-        svg.append("g")
-            .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x));
 
-        // add the y Axis
-        svg.append("g")
-            .call(d3.axisLeft(y));
-
+    // add the y Axis
+    svg.append("g")
+        .call(d3.axisLeft(y));
 
 
 }
